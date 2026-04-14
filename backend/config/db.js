6ea08007c1aa0e248622a9a3d2a81886ffed9256
Thembelihle Sibiya <lihleabegailsibiya@gmail.com>
2026-04-14 22:11:02 +0200

    feat: final full stack ready for deployment (frontend + backend)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,28 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-async function connectToDB() {
-    try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mzansibuilds');
-    console.log('Connected to MongoDB successfully');
-} catch (error) { 
-     console.log('MongoDB connection failed:', error.message);  
+// async function connectToDB() {
+//     try {
+//     await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mzansibuilds');
+//     console.log('Connected to MongoDB successfully');
+// } catch (error) { 
+//      console.log('MongoDB connection failed:', error.message);  
 
-}
- };  
+// }
+//  };  
 
-module.exports = connectToDB;
+// module.exports = connectToDB;
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Atlas connected 🚀");
+  } catch (error) {
+    console.error("DB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
